9ce5a91b5ba975c230bfc7352955f08382950d92
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-08-27 11:23:19 +0530

    remaining js also done

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-document.querySelectorAll(".box").forEach(box=>{
-    box.addEventListener("click" , ()=>{
-        const link = box.getAttribute("data-link");
-        if (link) window.location.href = link;
-    })
+const AllBox = document.querySelectorAll(".box");
+const AllFullBox = document.querySelectorAll(".fullbox");
+
+AllFullBox.forEach(page => page.style.display = "none");
+
+AllBox.forEach((box,index)=>{
+        box.addEventListener("click" , ()=>{
+            AllFullBox.forEach(page => page.style.display = "none");
+            AllFullBox[index].style.display = "block";
+            AllFullBox[index].scrollIntoView({ behavior: "smooth" });
+        });
 });
